265530fef824361ecf76caf999ea8f5ffcbbda4d
Sara Ammourh <saraammourh814@gmail.com>
2025-09-07 23:10:17 +0300

    Created Factories to help with creating Strategies

---
+++ src/main/java/org/example/dataprocessor/analysis/AnalysisFactory.java
@@ -0,0 +1,23 @@
+package org.example.dataprocessor.analysis;
+import org.example.dataprocessor.enums.AnalysisType;
+
+public class AnalysisFactory {
+    public static IAnalysisType createAnalysisType(AnalysisType analysisType) {
+        if (analysisType == AnalysisType.MEAN) {
+            return new MeanAnalysis();
+        }
+        if (analysisType == AnalysisType.MEDIAN) {
+            return new MedianAnalysis();
+        }
+        if (analysisType == AnalysisType.STD_DEV) {
+            return new StandardDeviationAnalysis();
+        }
+        if (analysisType == AnalysisType.P90_NEAREST_RANK) {
+            return new P90NearestRankAnalysis();
+        }
+        if (analysisType == AnalysisType.TOP3_FREQUENT_COUNT_SUM) {
+            return new Top3FrequentCountSumAnalysis();
+        }
+        throw new RuntimeException("Invalid AnalysisType");
+    }
+}
---
+++ src/main/java/org/example/dataprocessor/cleaning/CleaningFactory.java
@@ -0,0 +1,14 @@
+package org.example.dataprocessor.cleaning;
+import org.example.dataprocessor.enums.CleaningType;
+
+public class CleaningFactory {
+    public static ICleaningType createCleaningType(CleaningType cleaningType) {
+        if (cleaningType == CleaningType.REMOVE_NEGATIVES) {
+            return new RemoveNegativesCleaning();
+        }
+        if (cleaningType == CleaningType.REPLACE_NEGATIVES_WITH_ZERO) {
+            return new ReplaceNegativesWithZeroCleaning();
+        }
+        throw new RuntimeException("Invalid CleaningType");
+    }
+}
